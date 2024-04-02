d829494bd74401032a5e6d9039f0dde779d43cee
janbridley <bradleyjenj@gmail.com>
2024-04-02 15:24:22 -0400

    Add NotImplementedError to parsemm

---
+++ parsnip/parsemm.py
@@ -0,0 +1,6 @@
+"""mmCIF parsing tools."""
+if __name__ == "__main__":
+    raise NotImplementedError(
+        "mmCIF functionality has not been implemented.\n"
+        "See https://github.com/glotzerlab/parsnip/issues/1 for more details."
+    )
