656e9e1b08e56ee3da2871edf53131ec432aec84
janbridley <bradleyjenj@gmail.com>
2024-04-05 12:17:29 -0400

    Add _str2num and _deg2rad _utils

--- parsnip/_utils.py
+++ parsnip/_utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class ParseWarning(Warning):
     def __init__(self, message):
         self.message = message
@@ -12,3 +15,13 @@ class ParseError(RuntimeError):
 
     def __str__(self):
         return repr(self.message)
+
+
+def _str2num(val: str):
+    """Convert a string value to an integer if possible, or a float otherwise."""
+    return float(val) if "." in val else int(val)
+
+
+def _deg2rad(val: float | int):
+    """Convert a value in degrees to one in radians."""
+    return val * np.pi / 180
