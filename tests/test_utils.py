e0b693f9f0468c6c7a84e9eacdf14074015fbfdd
janbridley <bradleyjenj@gmail.com>
2024-04-05 12:29:20 -0400

    Add tests for new utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from parsnip._utils import ParseError, ParseWarning
+from parsnip._utils import ParseError, ParseWarning, _deg2rad, _str2num
 
 
 def test_parse_error():
@@ -15,3 +16,21 @@ def test_parse_warning():
         raise ParseWarning("TEST_WARNING_RAISED")
 
     assert "TEST_WARNING_RAISED" in str(warning.value)
+
+
+def test_deg2rad(seed=43):
+    rng = np.random.default_rng(seed)
+    angles = rng.uniform(low=0, high=180, size=10_000)
+    np.testing.assert_allclose(
+        np.deg2rad(angles), [_deg2rad(val) for val in angles], atol=2e-15
+    )
+
+
+@pytest.mark.parametrize("string", ["3.1415926", "-12345", str(1e6), "0.00000003579"])
+def test_str2num(string):
+    converted_val = _str2num(string)
+    if "." in string:
+        assert isinstance(converted_val, float)
+    else:
+        assert isinstance(converted_val, int)
+    assert np.isclose(float(string), converted_val)
