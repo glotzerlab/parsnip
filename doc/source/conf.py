8d108e861574595243a797723f2a688691b44ea6
janbridley <bradleyjenj@gmail.com>
2024-04-07 15:37:10 -0400

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,11 +14,25 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "autodocsumm",
+]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
 
+autodoc_default_options = {
+    "inherited-members": True,
+    "show-inheritance": True,
+    "autosummary": True,
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
