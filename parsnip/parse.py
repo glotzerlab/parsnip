0c487089ee6b5d186878de16017cf848a9bc4f00
janbridley <bradleyjenj@gmail.com>
2024-04-25 16:01:01 -0400

    Typo fix

--- parsnip/parse.py
+++ parsnip/parse.py
@@ -238,7 +238,7 @@ def read_key_value_pairs(
     """Extract key-value pairs from a CIF file.
 
     By default, this function reads all keys and returns data values as strings. Setting
-    ``only_read_numerics`` to True will cause the program to cast data to a nunmeric
+    ``only_read_numerics`` to True will cause the program to cast data to a numeric
     type (float or int). However, keys that cannot be safely cast into a numeric are
     skipped.
 
