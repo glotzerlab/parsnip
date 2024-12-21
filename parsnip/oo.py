06fabc7555969f1e3fa4a5b9c03d4565f3a3ddc7
janbridley <bradleyjenj@gmail.com>
2024-12-20 20:01:16 -0500

    Add oo.py temp implementation

--- parsnip/oo.py
+++ parsnip/oo.py
@@ -7,25 +7,29 @@ import re
 import warnings
 
 from more_itertools import peekable
+import numpy as np
 
 from parsnip.parse import _parsed_line_generator
 
 NONTABLE_LINE_PREFIXES = ("_", "#")
 
 
-def is_key(line: str | None):
+def _is_key(line: str | None):
     return line is not None and line.strip()[:1] == "_"
 
 
-def is_data(line: str | None):
+def _is_data(line: str | None):
     return line is not None and line.strip()[:1] != "_"
 
 
-def strip_comments(s: str):
+def _strip_comments(s: str):
     return s.split("#")[0].strip()
 
+def _strip_quotes(s: str):
+    return s.replace("'", "").replace('"', "")
 
-def semicolon_to_string(line: str):
+
+def _semicolon_to_string(line: str):
     if "'" in line and '"' in line:
         warnings.warn(
             (
@@ -39,14 +43,33 @@ def semicolon_to_string(line: str):
     return line.replace(";", "'" if "'" not in line else '"')
 
 
-def line_is_continued(line: str):
+def _line_is_continued(line: str):
     return line.strip()[:1] == ";"
+
+def _try_cast_to_numeric(s: str):
+    """Attempt to cast a string to a number, returning the original string if invalid.
+
+    This method attempts to convert to a float first, followed by an int. Precision
+    measurements and indicators of significant digits are stripped.
+    """
+    parsed = re.match(r"\d+.?\d+", s)
+    if parsed is None:
+        return s
+    elif "." in parsed.group(0):
+        return float(parsed.group(0))
+    else:
+        return int(parsed.group(0))
 
 
 class CifFile:
     """Parsed CIF file."""
 
     def __init__(self, fn: str):
+        """Create a CifFile object from a filename.
+
+        On construction, the entire file is parsed into key-value pairs and data tables.
+        Comment lines are ignored.
+        """
         self._fn = fn
         self._pairs = {}
         self._tables = []
@@ -59,10 +82,41 @@ class CifFile:
 
     @property
     def pairs(self):
+        """A dict containing key-value pairs extracted from the file.
+
+        Numeric values will be parsed to int or float if possible. In these cases,
+        precision specifiers will be stripped.
+
+        Returns:
+        --------
+        dict[str : str | float | int]
+        """
         return self._pairs
 
     @property
     def tables(self):
+        """A list of data tables extracted from the file.
+
+        These are stored as tuples of (keys, data) where data is a numpy array.
+
+        Returns:
+        --------
+        list[(list[str], :math:`(N, N_{keys})` :class:`numpy.ndarray[str]`)]:
+            A list of tuples corresponding with the keys and data of the system.
+        """
+        return self._tables
+
+    @property
+    def table_data(self):
+        """Data from tables extracted from the file.
+
+        This is *only the data* from the :meth:`~.tables` property.
+
+        Returns:
+        --------
+        list[:math:`(N, N_{keys})` :class:`numpy.ndarray[str]`]:
+            A list of arrays corresponding with the data of the system.
+        """
         return self._tables
 
     PATTERNS = {
@@ -74,7 +128,13 @@ class CifFile:
         "space_delimited_data": r"'[^']*'|\"[^\"]*\"|\S+",
     }
 
+    def __getitem__(self, key: str):
+        """Return an item from the dictionary of key-value pairs."""
+        return self.pairs[key]
+
+
     def _parse(self):
+        """Parse the cif file into python objects."""
         data_iter = peekable(self._data.split("\n"))
 
         for line in data_iter:
@@ -82,12 +142,12 @@ class CifFile:
                 break  # Exit without StopIteration
 
             # Combine nonsimple data values into a single, parseable line ==============
-            while line_is_continued(data_iter.peek()):
-                line += strip_comments(next(data_iter))
-            line = semicolon_to_string(line)
+            while _line_is_continued(data_iter.peek()):
+                line += _strip_comments(next(data_iter))
+            line = _semicolon_to_string(line)
 
             # Skip processing if the line contains no data =============================
-            if line == "" or strip_comments(line) == "":
+            if line == "" or _strip_comments(line) == "":
                 continue
 
             # TODO: could support multi-block files in the future ======================
@@ -98,7 +158,9 @@ class CifFile:
             # Extract key-value pairs and save to the internal state ===================
             pair = self._cpat["key_value_general"].match(line)
             if pair is not None:
-                self._pairs.update({pair.groups()[0]: pair.groups()[1]})
+                self._pairs.update(
+                    {pair.groups()[0]: _try_cast_to_numeric(_strip_quotes(pair.groups()[1]))}
+            )
 
             # Build up tables by incrementing through the iterator =====================
             table = re.match(self._cpat["table_delimiter"], line)
@@ -109,37 +171,37 @@ class CifFile:
                 # First, extract table headers. Must be prefixed with underscore
                 line_groups = table.groups()
                 if line_groups[-1] != "":  # Extract table keys from the _loop line
-                    fragment = strip_comments(line_groups[-1].strip())
+                    fragment = _strip_comments(line_groups[-1].strip())
                     if fragment[:1] == "_":
                         keys = self._cpat["key_list"].findall(fragment)
                         table_keys.extend([] if keys is None else keys)
                     else:
                         continue
 
-                while is_key(data_iter.peek(None)):
-                    line = strip_comments(next(data_iter))
+                while _is_key(data_iter.peek(None)):
+                    line = _strip_comments(next(data_iter))
                     table_keys.extend(self._cpat["key_list"].findall(line))
 
-                while is_data(data_iter.peek(None)):
-                    line = strip_comments(next(data_iter))
+                while _is_data(data_iter.peek(None)):
+                    line = _strip_comments(next(data_iter))
                     parsed_line = self._cpat["space_delimited_data"].findall(line)
                     table_data.extend([parsed_line] if parsed_line else [])
 
-                print("KEYS:", table_keys)
-                print("VALS:", table_data)
+                # print("KEYS:", table_keys)
+                # print("VALS:", table_data)
+
+                self.tables.append((table_keys, np.atleast_2d(table_data)))
 
             if data_iter.peek(None) is None:
                 break
-        [print(pair) for pair in self._pairs.items()]
+        # [print(pair) for pair in self._pairs.items()]
 
 
 fn = "tests/sample_data/B-IncStrDb_Ccmm.cif"
 gen = _parsed_line_generator(fn, regexp=".*")
 
 cf = CifFile(fn=fn)
-# print(cf._data)
+[print(pair) for pair in cf.pairs.items()]
 
 
-# print([*gen])
-# print()
-# print([*gen])
+# print(cf.tables[0])
