5eb4bbfa0788d3b2b37e5da7fb7a33b067270dc7
janbridley <bradleyjenj@gmail.com>
2024-12-31 16:37:35 -0500

    Auto detect cif keys

--- tests/test_key_reader.py
+++ tests/test_key_reader.py
@@ -1,6 +1,8 @@
 import numpy as np
+from CifFile import CifFile as pycifRW
 from conftest import bad_cif, cif_files_mark, random_keys_mark
 from gemmi import cif
+from more_itertools import flatten
 
 
 def remove_invalid(s):
@@ -21,12 +23,14 @@ def _gemmi_read_keys(filename, keys, as_number=True):
 
 @cif_files_mark
 def test_read_key_value_pairs(cif_data):
-    parsnip_data = cif_data.file[cif_data.single_value_keys]
+    pycif = pycifRW(cif_data.filename).first_block()
+    invalid = [*flatten(pycif.loops.values()), *cif_data.failing]
+    all_keys = [key for key in pycif.true_case.values() if key.lower() not in invalid]
+
+    parsnip_data = cif_data.file[all_keys]
     for i, value in enumerate(parsnip_data):
-        assert cif_data.file[cif_data.single_value_keys[i]] == value
-    gemmi_data = _gemmi_read_keys(
-        cif_data.filename, keys=cif_data.single_value_keys, as_number=False
-    )
+        assert cif_data.file[all_keys[i]] == value
+    gemmi_data = _gemmi_read_keys(cif_data.filename, keys=all_keys, as_number=False)
     np.testing.assert_equal(parsnip_data, gemmi_data, verbose=True)
 
 
@@ -41,7 +45,7 @@ def test_read_key_value_pairs_random(cif_data, keys):
 
 
 def test_read_key_value_pairs_badcif(cif_data=bad_cif):
-    parsnip_data = cif_data.file[cif_data.single_value_keys]
+    parsnip_data = cif_data.file[cif_data.manual_keys]
     correct_data = [
         "1.000000(x)",
         "4.32343242",
