65a0015b7e10e9d5afad881cc52f4b50e08744b4
janbridley <bradleyjenj@gmail.com>
2024-12-31 21:07:19 -0500

    Verify all table content

--- tests/test_table_reader.py
+++ tests/test_table_reader.py
@@ -28,8 +28,13 @@ def test_reads_all_keys(cif_data):
 
     found_labels = [*flatten(cif_data.file.table_labels)]
     for key in all_keys:
-        assert key in found_labels, f"{found_labels}"
+        assert key in found_labels, f"Missing label: {found_labels}"
 
+    for loop in pycif.loops.values():
+        loop = [pycif.true_case[key] for key in loop]
+        parsnip_data = cif_data.file.get_from_tables(loop)
+        gemmi_data = _gemmi_read_table(cif_data.filename, loop)
+        np.testing.assert_array_equal(parsnip_data, gemmi_data)
 
 @cif_files_mark
 def test_read_symop(cif_data):
