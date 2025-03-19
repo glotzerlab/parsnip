88b0b3c8185b7560753a09a9086729a7eff63fd4
janbridley <bradleyjenj@gmail.com>
2025-03-19 16:27:45 -0400

    Include sympy in requirements and ci

--- .github/workflows/update-uv-lockfiles.sh
+++ .github/workflows/update-uv-lockfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for version in 3.{9..13}; do
-    uv pip compile --python-version="$version" pyproject.toml tests/requirements.in > ".github/requirements-$version.txt"
+    uv pip compile --python-version="$version" pyproject.toml requirements-sympy.txt tests/requirements.in > ".github/requirements-$version.txt"
 done
