18683763fae9fe6fa20e0f6ca01916c249b20e93
janbridley <bradleyjenj@gmail.com>
2025-04-09 11:48:51 -0400

    Bump lockfiles and add missing sympy

--- .github/workflows/update-uv-lockfiles.sh
+++ .github/workflows/update-uv-lockfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for version in 3.{9..13}; do
+for version in 3.{8..13}; do
     uv pip compile --python-version="$version" pyproject.toml requirements-sympy.txt tests/requirements.in > ".github/requirements-$version.txt"
 done
