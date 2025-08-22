03787a5ed98e363679d517aeb86b227bf4381b24
janbridley <bradleyjenj@gmail.com>
2025-08-22 10:36:03 -0400

    Reorder bracketed regex to match other keys

--- parsnip/parsnip.py
+++ parsnip/parsnip.py
@@ -938,7 +938,7 @@ class CifFile:
         return f"CifFile(fn={self._fn}) : {n_pairs} data entries, {n_tabs} data loops"
 
     PATTERNS: ClassVar = {
-        "key_value_general": rf"^(_[\w\.\-/\[\d\]]+)\s{_PROG_PLUS}([^#]{_PROG_PLUS})",
+        "key_value_general": rf"^(_[\w\.\-/\d\[\]]+)\s{_PROG_PLUS}([^#]{_PROG_PLUS})",
         "loop_delimiter": rf"(loop_){_WHITESPACE}{_PROG_STAR}([^\n]{_PROG_STAR})",
         "block_delimiter": rf"(data_){_WHITESPACE}{_PROG_STAR}([^\n]{_PROG_STAR})",
         "key_list": rf"_[\w_\.]{_PROG_PLUS}[\d\[\]]{_PROG_STAR}",
