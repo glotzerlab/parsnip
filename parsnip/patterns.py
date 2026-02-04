Fix normalizing hall inputs

parsnip/patterns.py
@@ -20,25 +20,38 @@ from typing import Literal, TypeVar
 import numpy as np
 from numpy.typing import ArrayLike
 
+
+def _normalize(string: str | None):
+    """Normalize a lookup by stripping spaces and matched quotes."""
+    if string is None:
+        return ""
+    string = string.strip()
+    if string[0] == '"' and string[-1] == '"':
+        string = string.strip('"')
+    elif string[0] == "'" and string[-1] == "'":
+        string = string.strip("'")
+    return re.sub(r"[\s;]", "", string)
+
+
+def _normalize_hall(string: str | None):
+    """Normalize a lookup by stripping spaces and matched quotes."""
+    if string is None:
+        return ""
+    string = string.strip()
+    if string[0] == '"' and string[-1] == '"':
+        string = string.strip('"')
+    elif string[0] == "'" and string[-1] == "'":
+        string = string.strip("'")
+    return string
+
+
 with open(Path(__file__).parent / "symops.json") as f:
-
-    def _normalize(string: str | None):
-        """Normalize a lookup by stripping spaces and matched quotes."""
-        if string is None:
-            return ""
-        string = string.strip()
-        if string[0] == '"' and string[-1] == '"':
-            string = string.strip('"')
-        elif string[0] == "'" and string[-1] == "'":
-            string = string.strip("'")
-        return re.sub(r"[\s;]", "", string)
-
     # Process to extract the required data, in the specific format we need
     _full_dict = {
         k: v | {"symops": np.asarray(v["symops"])[:, None]}
         for (k, v) in json.load(f).items()
     }
-    SYMOPS_BY_HALL = {_normalize(k): v["symops"] for k, v in _full_dict.items()}
+    SYMOPS_BY_HALL = {_normalize_hall(k): v["symops"] for k, v in _full_dict.items()}
     SYMOPS_BY_HM = {
         _normalize(k): v["symops"]
         for v in _full_dict.values()
@@ -307,9 +320,8 @@ def _lookup_symops(cif) -> np.ndarray | None:
     - _symmetry_Int_Tables_number    # Deprecated, ambiguous setting
     """
     symops = None
-    hall, hm, it = None, None, None
     if (hall := cif["_space_group_name_Hall"]) is not None:
-        symops = SYMOPS_BY_HALL.get(_normalize(hall))
+        symops = SYMOPS_BY_HALL.get(_normalize_hall(hall))
 
     if symops is None and (
         hm := cif["_space_group_name_H-M_alt"] or cif["_symmetry_space_group_name_H-M"]