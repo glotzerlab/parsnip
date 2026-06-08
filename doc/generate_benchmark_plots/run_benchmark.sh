978560372ff985aa70e7b7aae6a3cb740e41f57d
janbridley <bradleyjenj@gmail.com>
2026-06-08 10:03:11 -0400

    Add benchmark runner

---
+++ doc/generate_benchmark_plots/run_benchmark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) 2025-2026, The Regents of the University of Michigan
+# This file is from the parsnip project, released under the BSD 3-Clause License.
+
+set -euo pipefail
+
+REPO_ROOT="$(pwd)"
+PLOT_DIR="$REPO_ROOT/doc/generate_benchmark_plots"
+BENCH_DIR="$(mktemp -d)"
+trap 'rm -rf "$BENCH_DIR"' EXIT
+
+echo "Cloning cif-parsing-benchmark into $BENCH_DIR"
+git clone git@github.com:janbridley/cif-parsing-benchmark.git "$BENCH_DIR"
+cd "$BENCH_DIR"
+
+uv pip install -r requirements.txt
+tar -xf structures.tar.xz
+echo "Running benchmarks..." && bash benchmark.sh
+
+python "$PLOT_DIR/plot.py" --out "$PLOT_DIR/benchmark_105.svg"
+
+echo "... saved to $PLOT_DIR/benchmark_105.svg"
